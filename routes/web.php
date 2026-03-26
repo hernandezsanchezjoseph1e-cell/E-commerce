da64e1f5e1b6f3e01e0cab36df2ea2ee85aa718d
Joshua Mejia <l22270436@tuxtla.tecnm.mx>
2026-03-26 13:15:17 -0600

    Actualización de dashboards y navbar de usuario cliente y empleado

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,16 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth'])->group(function () {
 
+    Route::prefix('cliente')->middleware('role:cliente')->group(function () {
+        Route::get('/dashboard', fn() => view('cliente.dashboard'))
+            ->name('dashboard.cliente');
+    });
+
+    Route::prefix('empleado')->middleware('role:empleado')->group(function () {
+        Route::get('/dashboard', fn() => view('empleado.dashboard'))
+            ->name('dashboard.empleado');
+    });
+
     Route::prefix('gerente')->middleware('role:gerente')->group(function () {
         Route::get('/dashboard', fn() => view('gerente.dashboard'))
             ->name('dashboard.gerente');
